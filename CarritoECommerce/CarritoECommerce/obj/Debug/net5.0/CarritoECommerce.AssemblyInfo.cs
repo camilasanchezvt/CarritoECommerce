c2008e48f21f9d4099a61abb68de870ec1c0b844
yuli <giulianamanzo1199@gmail.com>
2025-09-21 22:17:02 -0300

    agregue carpetas, archivos e interfaces

--- CarritoECommerce/CarritoECommerce/obj/Debug/net5.0/CarritoECommerce.AssemblyInfo.cs
+++ CarritoECommerce/CarritoECommerce/obj/Debug/net5.0/CarritoECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarritoECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93db79c214764694382dc069169224cbc940ec72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarritoECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarritoECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
