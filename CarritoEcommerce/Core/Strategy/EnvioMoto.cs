79d76d06244f74be1593b2ad6e1f7a963a40b073
camilasanchezvt <camilaxsanchez6@gmail.com>
2025-10-04 16:45:46 -0300

    chore: subida inicial del proyecto

---
+++ CarritoEcommerce/Core/Strategy/EnvioMoto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Etapa2Envios.Core.Singleton;
+
+
+namespace Etapa2Envios.Core.Strategy
+{
+        public class EnvioMoto : IEnvioStrategy
+        {
+            public string Nombre => "Moto";
+
+            public decimal Calcular(decimal subtotal)
+            {
+                return 1200m;
+            }
+        }
+
+}
---
+++ CarritoEcommerce/Core/Strategy/RetiroEnTienda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Etapa2Envios.Core.Singleton;
+
+
+namespace Etapa2Envios.Core.Strategy
+{
+        public class RetiroEnTienda : IEnvioStrategy
+        {
+            public string Nombre => "Retiro";
+
+            public decimal Calcular(decimal subtotal)
+            {
+                return 0;
+            }
+        }
+}
