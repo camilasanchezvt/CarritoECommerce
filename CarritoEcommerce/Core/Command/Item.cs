5f7759bce9426e735fea179ff640afd0c4456d96
yuli <giulianamanzo1199@gmail.com>
2025-10-06 13:04:12 -0300

    feat: añadiendo clase item, cart e interfaces

---
+++ CarritoEcommerce/Core/Command/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarritoECommerce.Command
+{
+    public class Item
+    {
+        public string Sku { get; init; }
+        public string Name { get; init; } = "";
+        public decimal Price { get; init; }
+        public int Quantity { get; set; }
+    }
+}
+}
---
+++ CarritoEcommerce/Core/Interfaces/ICartPort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CarritoECommerce.Core.Interfaces
+{
+    public interface ICartPort
+    {
+        decimal Subtotal(); // suma el total (precio * cantidad)
+        void Run(ICommand cmd); // ejecuta un comando y lo guarda en historial
+        void Undo(); // deshacer último comando
+        void Redo(); // rehace el ultimo comando deshecho
+    }
+}
